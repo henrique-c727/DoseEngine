3aa911c4846d162f791ba40cb0894cb684e986ee
Henrique <henrique.carneiro727@gmailcom>
2026-07-07 18:37:29 +0100

    config.py atualizado

---
+++ dose_engine/config.py
@@ -0,0 +1,17 @@
+GRID = {
+    "dx": 0.2,
+    "dz": 0.2,
+    "nx": 100,
+    "nz": 200
+}
+
+BEAM = { 
+    "energy_mv": 6,
+    "field_size_cm": 10.0, 
+    "ssd_cm": 100.0 
+}
+
+ENGINE = {
+    "model_type": "pencil_beam", 
+    "apply_etar": False, 
+    "etar_sigma": 1.0 }
