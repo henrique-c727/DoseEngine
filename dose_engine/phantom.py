d86c8430915bbcace04bb77f8cc57be882a8a82f
Henrique <henrique.carneiro727@gmailcom>
2026-07-09 10:46:32 +0100

    Alguns testes adicionados

--- dose_engine/phantom.py
+++ dose_engine/phantom.py
@@ -28,3 +28,31 @@ def tissue_insert(phantom, zi_cm, zf_cm, xi_cm, xf_cm, density):
 
     return phantom
 
+# Testes
+
+def create_lung():
+
+    phantom_lung = water_phantom()
+    phantom_lung = tissue_insert(
+        phantom_lung,
+        zi_cm = 5.0,
+        zf_cm = 15.0,
+        xi_cm = 0.0,
+        xf_cm = GRID["nx"] * GRID["dx"],
+        density=0.3
+        )
+    
+    return phantom_lung
+
+def create_bone():
+    phantom_bone = water_phantom()
+    phantom_bone = tissue_insert(
+        phantom_bone,
+        zi_cm = 10.0,
+        zf_cm = 12.0,
+        xi_cm = 0.0,
+        xf_cm = GRID["nx"] * GRID["dx"],
+        density=1.8
+        )
+    
+    return phantom_bone
