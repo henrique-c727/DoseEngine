5a34d6a875dbbf98d357f86aed2b3dfb21171312
Henrique <henrique.carneiro727@gmailcom>
2026-07-09 11:01:20 +0100

    Alguns comentarios adicionados para clarificar variáveis

--- dose_engine/config.py
+++ dose_engine/config.py
@@ -1,14 +1,14 @@
 GRID = {
-    "dx": 0.2,
-    "dz": 0.2,
-    "nx": 100,
-    "nz": 200
+    "dx": 0.2, # pixel horizontal length in cm 
+    "dz": 0.2, # pixel vertical length in cm
+    "nx": 100, # total number of pixels horizontally
+    "nz": 200 # total number of pixels vertically
 }
 
 BEAM = { 
     "energy_mv": 6,
     "field_size_cm": 10.0, 
-    "ssd_cm": 100.0 
+    "ssd_cm": 100.0 # source to surface distance in cm
 }
 
 ENGINE = {
@@ -17,4 +17,3 @@ ENGINE = {
     "etar_sigma": 1.0
 }
 
-#
