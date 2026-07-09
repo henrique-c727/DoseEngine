4fb59c3aea4c7d1ccd7d5b780c5eef8648efb1c1
Henrique <henrique.carneiro727@gmail.com>
2026-07-09 18:37:43 +0100

    Versão inical do main.py

--- dose_engine/main.py
+++ dose_engine/main.py
@@ -1,3 +1,73 @@
 import matplotlib
 matplotlib.use("TkAgg")
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from config import GRID
+import phantom as ph
+from engine import DoseEngine
+
+def plot_results(density_matrix, TERMA_matrix, dose_matrix):
+    width_cm = GRID["nx"] * GRID["dx"]
+    depth_cm = GRID["nz"] * GRID["dz"]
+    extent = [0, width_cm, depth_cm, 0]
+
+    fig, axs = plt.subplots(2, 2, figsize=(14,10), constrained_layout=True)
+    fig.suptitle("DoseEngine v1.0 - Beam analysis (Pencil Beam)", fontsize=16, fontweight="bold")
+
+    # CT
+    im1 = axs[0,0].imshow(density_matrix, cmap="bone", extent=extent, vmin=0, vmax=2.0, aspect="auto")
+    axs[0,0].set_title("Effective density")
+    fig.colorbar(im1, ax=axs[0,0], label="Relative density")
+
+    # TERMA
+    im2 = axs[0,1].imshow(TERMA_matrix, cmap="magma", extent=extent, aspect="auto")
+    axs[0,1].imshow(im2, ax=axs[0,1], label="Intensity")
+
+    # FINAL DOSE
+    im3 = axs[1,0].imshow(dose_matrix, cmap="turbo", extent=extent, aspect="auto")
+    axs[1,0].set_title("Final Dose Distribution")
+    fig.colorbar(im3, ax=axs[1,0], label="Relative dose")
+
+    # PDD and LATERAL PROFILE
+    z_axis = np.arange(GRID["nz"]) * GRID["dz"]
+    x_axis=np.arange(GRID["nx"]) * GRID["dx"]
+
+    pdd = dose_matrix[:, GRID["nx"] // 2] # midpoint of the lung
+    lateral_profile = dose_matrix[int(10 / GRID["dz"]), :]
+
+    axs[1,1].plot(z_axis, pdd, color="red", linewidth=2, label="PDD (central axis)")
+    axs[1,1].axvspan(5.0, 15.0, color="gray", alpha=0.2, label="Lung")
+
+    ax_secundary = axs[1,1].twiny()
+    ax_secundary.plt(x_axis, lateral_profile, color="blue", linewidth=2, linestyle="--", label="Lateral Profile (Z = 10 cm)")
+
+    axs[1,1].set_title("Quantitative analysis (PDD and lateral profile")
+    axs[1,1].set_xlabel("Depth Z (cm) [Red line]")
+    ax_secundary.set_xlabel("Lateral X (cm) [Blue Line]")
+    axs[1,1].grid(True, linestyle="--", alpha=0.6)
+
+    lines1, labels1 = axs[1,1].get_legend_handels_labels()
+    lines2, labels2 = ax_secundary.get_legend_handels_labels()
+    axs[1,1].legend(lines1 + lines2, labels1 + labels2, loc="upper right")
+
+    plt.show()
+
+if __name__ == "__main__":
+    print("Inicializing DoseEngine...")
+
+    print("Generating anatomic phantom...")
+    pacient_lung = ph.create_lung
+
+    print("Calculating primary transport (TERMA)...")
+    engine_terma = DoseEngine(pacient_lung, model="simple")
+    visual_terma = engine_terma.run()
+
+    print("Calculating secundary transport (Pencil Beam Model)...")
+    engine_dose = DoseEngine(pacient_lung, model="pencil_beam")
+    final_dose = engine_dose.run()
+
+    print("Generating visualization elements...")
+    density_used = engine_dose.matrix_calculation
+    plot_results(density_used, visual_terma, final_dose)
