3fca4dd80ecc4f4735a6f87042c6e5e6d2af3c48
Henrique <henrique.carneiro727@gmail.com>
2026-07-12 21:41:35 +0100

    Adicionados comentários explicativos como docstring para não sobrecarregar o código

--- dose_engine/config.py
+++ dose_engine/config.py
@@ -1,19 +1,40 @@
+"""
+Global configuration for DoseEngine
+
+Spatial dimensions are expressed in centimeters
+
+Brief description:
+
+GRID
+dx and dz: voxel size in their respective dimensions
+nx and nz: number of voxels along their respective axis
+
+BEAM
+ssd: source to surface distance
+
+ENGINE
+apply_etar: Enables the qualitative ETAR-inspired density correction
+etar_sigma: Gaussian smoothing scale used by the correction
+"""
+
+
+
 GRID = {
-    "dx": 0.2, # pixel horizontal length in cm 
-    "dz": 0.2, # pixel vertical length in cm
-    "nx": 100, # total number of pixels horizontally
-    "nz": 200 # total number of pixels vertically
+    "dx": 0.2, 
+    "dz": 0.2, 
+    "nx": 100, 
+    "nz": 200  
 }
 
 BEAM = { 
-    "energy_mv": 6,
-    "field_size_cm": 10.0, 
-    "ssd_cm": 100.0 # source to surface distance in cm
+    "energy_mv": 6, # 
+    "field_size_cm": 10.0,
+    "ssd_cm": 100.0 
 }
 
 ENGINE = {
     "model_type": "pencil_beam", 
-    "apply_etar": False, 
-    "etar_sigma": 1.0
+    "apply_etar": False, # True enables correction
+    "etar_sigma": 1.0 
 }
 
