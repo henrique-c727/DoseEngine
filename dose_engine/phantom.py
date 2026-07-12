729625e393d03b52c1e422ef32ee9a43c7738cb4
Henrique <henrique.carneiro727@gmail.com>
2026-07-12 22:04:11 +0100

    Documentação das funções

--- dose_engine/phantom.py
+++ dose_engine/phantom.py
@@ -3,6 +3,15 @@ from config import GRID
 
 
 def water_phantom():
+    """
+    Creates a homogeneous 2D water phantom, represented by
+    a relative density matrix with all voxels set to 1.0,
+    corresponding to water.
+
+    Returns
+    -------
+    Phantom matrix.
+    """
 
     lines = GRID["nz"]
     columns = GRID["nx"]
@@ -11,7 +20,22 @@ def water_phantom():
     return phantom
 
 def tissue_insert(phantom, zi_cm, zf_cm, xi_cm, xf_cm, density):
+    """
+    Inserts a rectangular region of constant density into a phantom.
 
+    Parameters
+    ----------
+    phantom: 2D relative density matrix to be modified.
+
+    x, z: This set of parameters specificate the initial
+    and final coordinates of the inserted region in cm.
+
+    density: Relative density assigned to the inserted region.
+
+    Returns
+    -------
+    Modified phantom matrix.
+    """
 
     line_i = int(np.round(zi_cm / GRID["dz"]))
     line_f = int(np.round(zf_cm / GRID["dz"]))
@@ -34,6 +58,18 @@ def tissue_insert(phantom, zi_cm, zf_cm, xi_cm, xf_cm, density):
 # Testes
 
 def create_lung():
+    """
+    Creates a water phantom containing a lung-equivalent insertion.
+
+    The lung region extends originally from 5 cm to 15 cm in depth and occupies
+    the full lateral width of the phantom.
+
+    Intended to be modified by the user, if needed.
+
+    Returns 
+    -------
+    Relative density matrix containing a lung region with, originally, density 0.3
+    """
 
     phantom_lung = water_phantom()
     phantom_lung = tissue_insert(
@@ -49,6 +85,18 @@ def create_lung():
 
 
 def create_bone():
+    """
+    Creates a water phantom containing a bone-equivalent insertion.
+
+    The bone region originally extends from 10 cm to 12 cm in depth and occupies
+    the full lateral width of the phantom.
+
+    Intended to be modified by the user, if needed.
+
+    Returns
+    -------
+    Relative density matrix containing a bone region with, originally, density 1.8.
+    """
     phantom_bone = water_phantom()
     phantom_bone = tissue_insert(
         phantom_bone,
@@ -62,9 +110,19 @@ def create_bone():
     return phantom_bone
 
 def create_custom():
+    """
+    Creates an example phantom containing a high-density material.
+
+    The inserted slab is intended only as a demonstration of how custom
+    heterogeneous regions can be introduced into the computational grid.
+
+    Returns
+    -------
+    Relative density matrix containing, originally, a high-density region with
+    density 4.5.
+    """
     custom_phantom = water_phantom()
     
-    # For example, a titanium mass
     custom_phantom = tissue_insert(
         custom_phantom,
         zi_cm=8.0,
